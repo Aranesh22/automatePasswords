0bc44916077bffa961cac0a47a48a6862594fc8d
Aranesh22 <a.aranesh@gmail.com>
2021-06-22 00:32:38 -0400

    Create passwordGen.js

---
+++ passwordGen.js
@@ -0,0 +1,16 @@
+const request = new XMLHttpRequest();  
+request.open("GET","https://www.passwordrandom.com/query?command=password"); 
+request.send(); 
+request.onload = () => { 
+
+    if(request.status === 200) { 
+
+        console.log(JSON.parse(request.response));
+    } 
+    else { 
+        console.log("ERROR");
+    }
+
+
+
+}
