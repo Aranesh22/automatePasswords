591973f16a805bf1ca170f90477143876fad839e
Aranesh22 <a.aranesh@gmail.com>
2021-06-29 00:45:33 -0400

    Update passwordGen.js

--- passwordGen.js
+++ passwordGen.js
@@ -1,11 +1,27 @@
 const fetch = require('node-fetch'); 
-
-async function getURLS() { 
+let simplePassword = " "; 
+async function getSimplePassword(x) { 
     let res = await fetch('https://www.passwordrandom.com/query?command=password&format=json'); 
-    let data = await res.json(); 
-    return data; 
+    let data = await res.json();  
+    x += data.char[0];  
+
+
+    console.log(x); 
+
 }  
 
-getURLS().then(data => console.log(data.char));
 
-getURLS();
+async function getRandomWord() { 
+
+    let res = await fetch('https://random-words-api.vercel.app/word'); 
+    let data = await res.json();  
+    getSimplePassword(data[0].word);
+ 
+
+} 
+
+//getRandomWord().then(data => (simplePassword = data.word));  
+
+getRandomWord();
+
+//console.log(simplePassword);
