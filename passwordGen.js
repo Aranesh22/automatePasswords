417caf4758cd1c30d8b5eeb7b49f6fa743a095c5
Aranesh22 <a.aranesh@gmail.com>
2021-06-29 21:27:21 -0400

    Update passwordGen.js

--- passwordGen.js
+++ passwordGen.js
@@ -1,39 +1,22 @@
-const fetch = require('node-fetch'); 
 let simplePassword = " ";  
-
 const generator = require('generate-password');
-async function getSimplePassword(x) { 
-    let res = await fetch('https://www.passwordrandom.com/query?command=password&format=json'); 
-    let data = await res.json();  
-    x += data.char[0];  
+const randomWords = require('random-words');
+let x; 
+let  password 
+function genPass(x) {
+    password = generator.generate({
+        length: x,
+        numbers: true, 
+        symbols: true
+    }); 
+}
 
+let rWord =  randomWords();
+x = 64 -rWord.length; 
+genPass(x); 
+let combPasswrod = rWord + password;
 
-    console.log(x); 
-
-}  
-
-
-async function getRandomWord() { 
-
-    let res = await fetch('https://random-words-api.vercel.app/word'); 
-    let data = await res.json();  
-    console.log(data[0].word);
- 
-
-} 
-
-//getRandomWord().then(data => (simplePassword = data.word));  
-
-getRandomWord();
-
-//console.log(simplePassword); 
-
-
-var password = generator.generate({
-	length: 30,
-	numbers: true, 
-    symbols: true
-});
-
+console.log(combPasswrod + " " + combPasswrod.length); 
+//passLength(x);
 // 'uEyMTw32v9'
 //console.log(password);
